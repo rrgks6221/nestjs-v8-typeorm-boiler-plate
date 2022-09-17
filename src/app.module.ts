d819a4356a87abaeb99fe7787b7d119de5488475
LeeSeokHo <rrgks@naver.com>
2022-09-17 16:32:58 +0900

    Feat: HealthCheck module 추가 server, storage, memory

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppService } from '@src/app.service';
 import { AppController } from '@src/app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './modules/health/health.module';
 import Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env'],
-      validationSchema: Joi.object({ PORT: Joi.number().default(3000) }),
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        SERVER_URL: Joi.string().default('http://localhost:3000'),
+      }),
     }),
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/modules/health/health.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  DiskHealthIndicator,
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+  MemoryHealthIndicator,
+} from '@nestjs/terminus';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly http: HttpHealthIndicator,
+    private readonly configService: ConfigService,
+    private readonly disk: DiskHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      // server health check
+      () =>
+        this.http.pingCheck(
+          'server-health',
+          this.configService.get<string>('SERVER_URL'),
+        ),
+      // storage check 전체 용량의 50%
+      () =>
+        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+      // memory heap 체크 150MB
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+    ]);
+  }
+}
